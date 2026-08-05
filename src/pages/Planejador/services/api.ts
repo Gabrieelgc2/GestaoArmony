ea642d43d24c794045f6c3db1f435913ca2ae144
Gabrieelgc2 <gabrielao8@hotmail.com>
2026-08-05 15:25:22 -0300

    add: url API

--- src/pages/Planejador/services/api.ts
+++ src/pages/Planejador/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {supabase} from "@/supabaseClient";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || 'https://gestao-armony-backend.onrender.com',
 });
 
 api.interceptors.request.use(async (config) => {
